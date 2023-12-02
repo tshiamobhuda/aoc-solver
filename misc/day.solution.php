d6c73ea3980d5f24b28b0e4607343ff8467af4df
Tshiamo <tshiamobhuda@gmail.com>
2023-12-02 14:03:03 +0200

    Adds simple Solution boilerplate generator

---
+++ misc/day.solution.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Resource\Solution;
+
+use App\Core\Domain\PuzzleInput;
+use App\Core\Domain\SolutionInterface;
+
+class DayNum implements SolutionInterface
+{
+    public function partA(PuzzleInput $input): string
+    {
+        // TODO: Implement part() method.
+        return '';
+    }
+
+    public function partB(PuzzleInput $input): string
+    {
+        // TODO: Implement part() method.
+        return '';
+    }
+}
